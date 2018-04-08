7b9caa0412beedb768d93f8fd2041d8d4f3cfef8
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2018-04-08 17:32:15 +0900

    :new: Resistanceを上昇させるアクセサリー追加

---
+++ Assets/GL/Scripts/Battle/Accessories/AddStatusResistance.cs
@@ -0,0 +1,24 @@
+﻿using GL.Scripts.Battle.CharacterControllers;
+using GL.Scripts.Battle.Systems;
+using UnityEngine;
+
+namespace GL.Scripts.Battle.Accessories
+{
+    /// <summary>
+    /// 状態異常の耐性を上昇させるアクセサリー
+    /// </summary>
+    [CreateAssetMenu(menuName = "GL/Accessories/AddStatusResistance")]
+    public sealed class AddStatusResistance : Accessory
+    {
+        [SerializeField]
+        private Constants.StatusAilmentType statusAilmentType;
+
+        [SerializeField]
+        private float rate;
+
+        public override void OnStartBattle(Character equippedCharacter)
+        {
+            equippedCharacter.StatusController.AddResistanceToAccessory(this.statusAilmentType, this.rate);
+        }
+    }
+}
