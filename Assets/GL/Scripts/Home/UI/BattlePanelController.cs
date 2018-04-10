6e7797441ffad3a9203fc8cfeb344cd13339b821
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2018-04-11 02:01:07 +0900

    :new: 戦闘するパーティを選択出来るように

---
+++ Assets/GL/Scripts/Home/UI/BattlePanelController.cs
@@ -0,0 +1,29 @@
+﻿using GL.Scripts.Battle.PartyControllers.Blueprints;
+using UnityEngine;
+
+namespace GL.Scripts.Home.UI
+{
+    /// <summary>
+    /// バトルのパネルを制御するクラス
+    /// </summary>
+    public sealed class BattlePanelController : MonoBehaviour
+    {
+        [SerializeField]
+        private BattlePartySelectButtonController controllerPrefab;
+
+        [SerializeField]
+        private RectTransform scrollParent;
+
+        [SerializeField]
+        private Blueprint[] blueprints;
+
+        void Start()
+        {
+            foreach (var blueprint in this.blueprints)
+            {
+                var controller = Instantiate(this.controllerPrefab, this.scrollParent, false);
+                controller.Initialize(blueprint);
+            }
+        }
+    }
+}
---
+++ Assets/GL/Scripts/Home/UI/BattlePartySelectButtonController.cs
@@ -0,0 +1,31 @@
+﻿using GL.Scripts.Battle.PartyControllers.Blueprints;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GL.Scripts.Home.UI
+{
+    /// <summary>
+    /// バトルする敵パーティを選択するボタンを制御するクラス
+    /// </summary>
+    public sealed class BattlePartySelectButtonController : MonoBehaviour
+    {
+        [SerializeField]
+        private Button button;
+        
+        [SerializeField]
+        private Text text;
+
+        public void Initialize(Blueprint blueprint)
+        {
+            this.text.text = blueprint.PartyName;
+            this.button.OnClickAsObservable()
+                .Where(_ => this.button.isActiveAndEnabled)
+                .SubscribeWithState(this, (_, _this) =>
+                {
+                    Debug.Log("Battle! " + _this.text.text);
+                })
+                .AddTo(this);
+        }
+    }
+}
