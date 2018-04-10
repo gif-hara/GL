51192cdc96f43db772ee3007a7a5ac1fbe79c242
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2018-04-10 20:49:20 +0900

    :new: 回復コマンド実装

---
+++ Assets/GL/Scripts/Battle/Commands/Blueprints/Recovery.cs
@@ -0,0 +1,20 @@
+﻿using GL.Scripts.Battle.Commands.Implements;
+using UnityEngine;
+
+namespace GL.Scripts.Battle.Commands.Blueprints
+{
+    /// <summary>
+    /// 回復コマンドの設定データ.
+    /// </summary>
+    [CreateAssetMenu(menuName = "GL/Commands/Blueprints/Recovery")]
+    public sealed class Recovery : Blueprint
+    {
+        [SerializeField]
+        private Implements.Recovery.Parameter parameter;
+        
+        public override IImplement Create()
+        {
+            return new Implements.Recovery(this.parameter);
+        }
+    }
+}
---
+++ Assets/GL/Scripts/Battle/Commands/Implements/Recovery.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using GL.Scripts.Battle.CharacterControllers;
+using GL.Scripts.Battle.Systems;
+
+namespace GL.Scripts.Battle.Commands.Implements
+{
+    /// <summary>
+    /// 回復を行うコマンド.
+    /// </summary>
+    public sealed class Recovery : Implement<Recovery.Parameter>
+    {
+        public Recovery(Parameter parameter)
+            : base(parameter)
+        {
+        }
+
+        public override Constants.CommandType CommandType { get { return Constants.CommandType.Recovery; } }
+
+        public override void Invoke(Character invoker)
+        {
+            base.Invoke(invoker);
+            
+            var targets = BattleManager.Instance.Parties
+                .GetFromTargetPartyType(invoker, this.TargetPartyType)
+                .GetTargets(invoker, this.TargetType, c => c.StatusController.GetTotalParameter(Constants.StatusParameterType.HitPoint), true);
+            
+            // 対象全てが死亡していた場合は何もしない
+            if (targets.Find(t => !t.StatusController.IsDead) == null)
+            {
+                this.Postprocess(invoker)();
+                return;
+            }
+            invoker.StartAttack(() =>
+            {
+                targets.ForEach(t =>
+                {
+                    var amount = Calculator.GetRecoveryAmount(invoker, this.parameter.Rate);
+                    t.Recovery(amount);
+                });
+            }, this.Postprocess(invoker));
+        }
+
+        [Serializable]
+        public class Parameter : CommandParameter
+        {
+            /// <summary>
+            /// 倍率
+            /// </summary>
+            public float Rate;
+        }
+    }
+}
