2415b9ce122505c3e3581f6df5e14dcca2396713
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2018-04-10 22:18:59 +0900

    :new: 武器実装

---
+++ Assets/GL/Scripts/Battle/Weapons/Weapon.cs
@@ -0,0 +1,19 @@
+﻿using GL.Scripts.Battle.Commands.Blueprints;
+using UnityEngine;
+
+namespace GL.Scripts.Battle.Weapons
+{
+    /// <summary>
+    /// 武器クラス
+    /// </summary>
+    /// <remarks>
+    /// コマンドリストを担う
+    /// </remarks>
+    [CreateAssetMenu(menuName = "GL/Weapon")]
+    public sealed class Weapon : ScriptableObject
+    {
+        [SerializeField]
+        private Blueprint[] commands;
+        public Blueprint[] Commands { get { return commands; } }
+    }
+}
