ca9eb2306522c49981de1021d16abccd014625e7
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2018-04-11 21:00:19 +0900

    :new: 初期化時にメインパネルを配置するように

---
+++ Assets/GL/Scripts/Home/UI/MainPanelController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace GL.Scripts.Home.UI
+{
+    /// <summary>
+    /// メインパネルを制御するクラス
+    /// </summary>
+    public sealed class MainPanelController : MonoBehaviour
+    {
+        void Start()
+        {
+            var rectTransform = (RectTransform) this.transform;
+            var width = rectTransform.rect.width;
+            for (int i = 0; i < rectTransform.childCount; i++)
+            {
+                var child = (RectTransform) rectTransform.GetChild(i);
+                var pos = child.anchoredPosition;
+                pos.x = width * i;
+                child.anchoredPosition = pos;
+                child.gameObject.SetActive(true);
+            }
+        }
+    }
+}
