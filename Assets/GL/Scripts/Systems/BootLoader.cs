3a3c33d6ffaa19610b9be79e26fbd976f08ba4e8
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2018-04-11 22:53:00 +0900

    :new: BootLoader実装

---
+++ Assets/GL/Scripts/Systems/BootLoader.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using GL.Scripts.User;
+using UnityEngine;
+
+namespace GL.Scripts.Systems
+{
+    /// <summary>
+    /// 一番最初に処理するクラス
+    /// </summary>
+    [CreateAssetMenu(menuName = "GL/Systems/BootLoader")]
+    public sealed class BootLoader : ScriptableObject
+    {
+        [SerializeField]
+        private int targetFrameRate;
+
+        [SerializeField]
+        private Party initialParty;
+                
+        void OnEnable()
+        {
+#if UNITY_EDITOR
+            if (UnityEditor.EditorApplication.isPlayingOrWillChangePlaymode == false)
+            {
+                return;
+            }
+#endif
+
+            // FPS設定
+            {
+                Application.targetFrameRate = this.targetFrameRate;
+            }
+            
+            // ユーザーデータを読み込む
+            {
+                var userData = UserData.Load();
+                if (userData.IsEmpty)
+                {
+                    userData.Initialize(this.initialParty);
+                }
+            }
+        }
+    }
+}
