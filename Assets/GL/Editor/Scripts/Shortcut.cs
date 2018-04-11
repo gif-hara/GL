dec8e6b77b8f02a201d187980b7723c577178b92
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2018-04-11 22:52:33 +0900

    :new: ショートカット追加

---
+++ Assets/GL/Editor/Scripts/Shortcut.cs
@@ -0,0 +1,17 @@
+﻿using HK.Framework.Systems;
+using UnityEditor;
+
+namespace GL.Editor.Scripts
+{
+    public sealed class Shortcut
+    {
+        [MenuItem("GL/Clear SaveData")]
+        private static void ClearSaveData()
+        {
+            if (EditorUtility.DisplayDialog("セーブデータ削除", "本当に削除しますか？", "OK", "Cancel"))
+            {
+                SaveData.Clear();
+            }
+        }
+    }
+}
