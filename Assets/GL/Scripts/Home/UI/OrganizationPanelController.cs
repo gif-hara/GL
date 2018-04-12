37ba660808ef72c79620c105afb70428cdd38da4
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2018-04-13 07:32:28 +0900

    :new: 編成パネルを作成中

---
+++ Assets/GL/Scripts/Home/UI/OrganizationPanelController.cs
@@ -0,0 +1,39 @@
+﻿using GL.Scripts.User;
+using UnityEngine;
+
+namespace GL.Scripts.Home.UI
+{
+    /// <summary>
+    /// 編成パネルを制御するクラス
+    /// </summary>
+    public sealed class OrganizationPanelController : MonoBehaviour
+    {
+        [SerializeField]
+        private RectTransform playersParent;
+
+        [SerializeField]
+        private PlayerButtonController[] parties;
+
+        [SerializeField]
+        private PlayerButtonController playerButtonPrefab;
+        
+        void Start()
+        {
+            var userData = UserData.Instance;
+            this.CreatePlayersPanel(userData);
+            foreach (var userDataPlayer in userData.Players)
+            {
+                Debug.Log(userDataPlayer.Id);
+            }
+        }
+
+        private void CreatePlayersPanel(UserData userData)
+        {
+            foreach (var player in userData.Players)
+            {
+                var button = Instantiate(this.playerButtonPrefab, this.playersParent, false);
+                button.SetProperty(player);
+            }
+        }
+    }
+}
---
+++ Assets/GL/Scripts/Home/UI/PlayerButtonController.cs
@@ -0,0 +1,31 @@
+﻿using GL.Scripts.User;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GL.Scripts.Home.UI
+{
+    /// <summary>
+    /// ホームのプレイヤーボタンを制御するクラス
+    /// </summary>
+    public sealed class PlayerButtonController : MonoBehaviour
+    {
+        [SerializeField]
+        private Button button;
+        public Button Button { get { return button; } }
+
+        [SerializeField]
+        private Text text;
+
+        public void SetProperty(Player player)
+        {
+            this.text.text = player.Blueprint.CharacterName;
+            this.Button.OnClickAsObservable()
+                .SubscribeWithState(player, (_, p) =>
+                {
+                    Debug.Log(string.Format("Player = {0} Id = {1}", p.Blueprint.CharacterName, p.Id));
+                })
+                .AddTo(this);
+        }
+    }
+}
