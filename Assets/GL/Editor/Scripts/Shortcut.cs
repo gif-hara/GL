a97652d38e2e87801971d31d335768096c58fda4
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2018-04-14 13:12:22 +0900

    :new: ショートカット設定

--- Assets/GL/Editor/Scripts/Shortcut.cs
+++ Assets/GL/Editor/Scripts/Shortcut.cs
@@ -5,7 +5,7 @@ namespace GL.Editor.Scripts
 {
     public sealed class Shortcut
     {
-        [MenuItem("GL/Clear SaveData")]
+        [MenuItem("GL/Clear SaveData #&1")]
         private static void ClearSaveData()
         {
             if (EditorUtility.DisplayDialog("セーブデータ削除", "本当に削除しますか？", "OK", "Cancel"))
