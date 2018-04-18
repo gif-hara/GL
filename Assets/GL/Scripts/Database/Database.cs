d7ba23a3163aa0b3d55797bb56081a8347018c68
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2018-04-18 23:48:45 +0900

    :new: データベース実装

---
+++ Assets/GL/Scripts/Database/Database.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace GL.Scripts.Database
+{
+    /// <summary>
+    /// データベース
+    /// </summary>
+    [CreateAssetMenu(menuName = "GL/Database/Database")]
+    public class Database : ScriptableObject
+    {
+        public Database Instance { get; private set; }
+
+        [SerializeField]
+        private Weapon weapon;
+        public Weapon Weapon { get { return weapon; } }
+    }
+}
---
+++ Assets/GL/Scripts/Database/Weapon.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace GL.Scripts.Database
+{
+    /// <summary>
+    /// 武器データベース
+    /// </summary>
+    [CreateAssetMenu(menuName = "GL/Database/Weapon")]
+    public class Weapon : ScriptableObject
+    {
+        [SerializeField]
+        private Battle.Weapons.Weapon[] list;
+
+        void Reset()
+        {
+            this.list = AssetDatabase.FindAssets("t:Weapon", new[] {"Assets/GL/MasterData/Weapons/Player"})
+                .Select(AssetDatabase.GUIDToAssetPath)
+                .Select(AssetDatabase.LoadAssetAtPath<Battle.Weapons.Weapon>)
+                .ToArray();
+        }
+    }
+}
