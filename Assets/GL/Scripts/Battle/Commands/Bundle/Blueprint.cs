9e284aea82a83aea706b64edad44c85a09bc5b96
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2018-07-29 23:02:02 +0900

    :new: コマンドをまとめるクラスを実装

---
+++ Assets/GL/Scripts/Battle/Commands/Bundle/Blueprint.cs
@@ -0,0 +1,16 @@
+﻿using GL.Scripts.Battle.Systems;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace GL.Scripts.Battle.Commands.Bundle
+{
+    /// <summary>
+    /// <see cref="Commands.Blueprints"/>をまとめている実際に実行されるコマンドの設計図
+    /// </summary>
+    [CreateAssetMenu(menuName = "GL/Commands/Bundle")]
+    public sealed class Blueprint : ScriptableObject
+    {
+        [SerializeField]
+        private Implement.Parameter parameter;
+    }
+}
---
+++ Assets/GL/Scripts/Battle/Commands/Bundle/Implement.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using GL.Scripts.Battle.Systems;
+using HK.Framework.Text;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace GL.Scripts.Battle.Commands.Bundle
+{
+    /// <summary>
+    /// <see cref="Commands.Blueprints"/>をまとめている実際に実行されるコマンドの実体
+    /// </summary>
+    public sealed class Implement
+    {
+        private Parameter parameter;
+
+        public string Name
+        {
+            get
+            {
+                return this.parameter.Name.Get;
+            }
+        }
+
+        public Implement(Parameter parameter)
+        {
+            this.parameter = parameter;
+        }
+
+        [Serializable]
+        public class Parameter
+        {
+            /// <summary>
+            /// コマンド名
+            /// </summary>
+            public StringAsset.Finder Name;
+
+            /// <summary>
+            /// 対象となるパーティタイプ
+            /// </summary>
+            public Constants.TargetPartyType TargetPartyType;
+
+            /// <summary>
+            /// ターゲットタイプ
+            /// </summary>
+            /// <remarks>
+            /// 誰を狙うかを決めるのに必要
+            /// </remarks>
+            public Constants.TargetType TargetType;
+
+            /// <summary>
+            /// 実行されるコマンドリスト
+            /// </summary>
+            public Blueprints.Blueprint[] elements;
+        }
+    }
+}
