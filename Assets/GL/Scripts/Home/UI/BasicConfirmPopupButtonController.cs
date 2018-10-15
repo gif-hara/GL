af3bf006a324c30cf8436aa4d0f201f9ebfd5ac7
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2018-10-15 21:06:36 +0900

    :new: シンプルなポップアップを実装

---
+++ Assets/GL/Scripts/Home/UI/BasicConfirmPopupButtonController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
+
+namespace GL.Home.UI
+{
+    /// <summary>
+    /// シンプルなポップアップのボタンを制御するクラス
+    /// </summary>
+    public sealed class BasicConfirmPopupButtonController : MonoBehaviour
+    {
+        [SerializeField]
+        private Button button;
+        public Button Button => this.button;
+
+        [SerializeField]
+        private Text text;
+        public Text Text => this.text;
+    }
+}
---
+++ Assets/GL/Scripts/Home/UI/BasicConfirmPopupController.cs
@@ -0,0 +1,44 @@
+﻿using UniRx;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
+
+namespace GL.Home.UI
+{
+    /// <summary>
+    /// シンプルなポップアップを制御するクラス
+    /// </summary>
+    public sealed class BasicConfirmPopupController : MonoBehaviour
+    {
+        [SerializeField]
+        private BasicConfirmPopupButtonController buttonPrefab;
+
+        [SerializeField]
+        private Transform buttonParent;
+
+        [SerializeField]
+        private Text title;
+
+        [SerializeField]
+        private Text message;
+
+        public Subject<int> Submit { get; private set; } = new Subject<int>();
+
+        public void Setup(string title, string message, params string[] buttonNames)
+        {
+            this.title.text = title;
+            this.message.text = message;
+            
+            for (var i = 0; i < buttonNames.Length; i++)
+            {
+                var button = Instantiate(this.buttonPrefab, this.buttonParent, false);
+                button.Button.OnClickAsObservable()
+                    .SubscribeWithState(this, (_, _this) =>
+                    {
+                        _this.Submit.OnNext(i);
+                    });
+                button.Text.text = buttonNames[i];
+            }
+        }
+    }
+}
