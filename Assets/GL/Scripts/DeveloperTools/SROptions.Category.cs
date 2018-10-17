bcb727c158e1aeb0dc435277af7ec7518ba8fdda
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2018-10-18 00:43:57 +0900

    :new: SROptions追加オプションを実装

---
+++ Assets/GL/Scripts/DeveloperTools/SROptions.Category.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+/// <summary>
+/// SROptionsのカテゴリー
+/// </summary>
+public partial class SROptions
+{
+    public static class Category
+    {
+        public const string UserData = "UserData";
+    }
+}
---
+++ Assets/GL/Scripts/DeveloperTools/SROptions.UserData.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using GL.User;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+/// <summary>
+/// <see cref="UserData"/>用のデバッグ
+/// </summary>
+public partial class SROptions
+{
+    [Category(Category.UserData)]
+    [Sort(100)]
+    public int Gold => UserData.Instance.Wallet.Gold;
+
+    [Category(Category.UserData)]
+    [Sort(200)]
+    public void AddGold()
+    {
+        const int value = 1000000;
+        UserData.Instance.Wallet.AddFromGold(value);
+        UserData.Instance.Save();
+        Debug.Log($"Added Gold {value}");
+    }
+}
