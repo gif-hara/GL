f1a071649e26202d64a8758b6a570661b5bbae51
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2018-10-30 23:56:38 +0900

    :syringe: ラベル間違っている

--- Assets/GL/Scripts/Database/MasterData/CharacterList.cs
+++ Assets/GL/Scripts/Database/MasterData/CharacterList.cs
@@ -9,7 +9,7 @@ namespace GL.Database
     [CreateAssetMenu(menuName = "GL/MasterData/List/Character")]
     public class CharacterList : MasterDataRecordList<CharacterRecord>
     {
-        protected override string FindAssetsFilter => "l:GK.Battle.CharacterControllers.Blueprint";
+        protected override string FindAssetsFilter => "l:GL.CharacterRecord";
 
         protected override string[] FindAssetsPaths => new[] { "Assets/GL/MasterData/Characters/Player" };
     }
