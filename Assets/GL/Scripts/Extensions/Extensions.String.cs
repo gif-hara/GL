4c30c445ef037b78506b81beeaa54e3d8bbb2dd5
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2018-10-30 23:58:00 +0900

    :new: Extensions.String追加

---
+++ Assets/GL/Scripts/Extensions/Extensions.String.cs
@@ -0,0 +1,16 @@
+﻿namespace GL.Extensions
+{
+    /// <summary>
+    /// <see cref="string"/>の拡張クラス
+    /// </summary>
+    public static partial class Extensions
+    {
+        /// <summary>
+        /// 改行コードを削除した文字列を返す
+        /// </summary>
+        public static string RemoveNewLine(this string self)
+        {
+            return self.Replace("¥r", "").Replace("¥n", "").Replace("\r", "").Replace("\n", "");
+        }
+    }
+}
