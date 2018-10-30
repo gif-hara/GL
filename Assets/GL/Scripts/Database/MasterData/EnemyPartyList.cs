8ded86706168a1a6e569bdf761c2a8346a04a7a8
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2018-10-31 00:14:15 +0900

    :syringe: ラベル間違っている

--- Assets/GL/Scripts/Database/MasterData/EnemyPartyList.cs
+++ Assets/GL/Scripts/Database/MasterData/EnemyPartyList.cs
@@ -9,7 +9,7 @@ namespace GL.Database
     [CreateAssetMenu(menuName = "GL/MasterData/List/EnemyParty")]
     public sealed class EnemyPartyList : MasterDataRecordList<PartyRecord>
     {
-        protected override string FindAssetsFilter => "l:GK.Battle.PartyControllers.Blueprint";
+        protected override string FindAssetsFilter => "l:GL.PartyRecord";
 
         protected override string[] FindAssetsPaths => new[] { "Assets/GL/MasterData/Parties/Enemy" };
     }
