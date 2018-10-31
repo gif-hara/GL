7e0c244a4ce69941d705018b004d71605521044c
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2018-10-31 22:09:21 +0900

    :new: 固定値回復コマンドを実装

---
+++ Assets/GL/Scripts/Battle/Commands/Element/Blueprints/RecoveryFixed.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace GL.Battle.Commands.Element.Blueprints
+{
+    /// <summary>
+    /// 固定値回復コマンドの設定データ.
+    /// </summary>
+    [CreateAssetMenu(menuName = "GL/Commands/Blueprints/RecoveryFixed")]
+    public sealed class RecoveryFixed : Blueprint
+    {
+        [SerializeField]
+        private Implements.RecoveryFixed.Parameter parameter;
+        
+        public override IImplement Create()
+        {
+            return new Implements.RecoveryFixed(this.parameter);
+        }
+
+#if UNITY_EDITOR
+        public override string FileName => $"RecoveryFixed_{this.parameter.Value}";
+#endif
+    }
+}
---
+++ Assets/GL/Scripts/Battle/Commands/Element/Implements/RecoveryFixed.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using GL.Battle.CharacterControllers;
+using GL.Battle;
+using HK.GL.Extensions;
+
+namespace GL.Battle.Commands.Element.Implements
+{
+    /// <summary>
+    /// 固定値回復を行うコマンド.
+    /// </summary>
+    public sealed class RecoveryFixed : Implement<RecoveryFixed.Parameter>
+    {
+        public RecoveryFixed(Parameter parameter)
+            : base(parameter)
+        {
+        }
+
+        public override bool TakeDamage
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public override void Invoke(Character invoker, Commands.Bundle.Implement bundle, Character[] targets)
+        {
+            targets.ForEach(t =>
+            {
+                t.Recovery(this.parameter.Value);
+            });
+        }
+
+        [Serializable]
+        public class Parameter : IParameter
+        {
+            /// <summary>
+            /// 回復値
+            /// </summary>
+            public int Value;
+        }
+    }
+}
