c5a6a861e7f6a36fe72c64f4bafba411779ce0c5
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2018-12-10 21:47:09 +0900

    :new: アセットインポート

---
+++ Assets/GL/Scripts/DeveloperTools/Importer/AllImporter.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace GL.DeveloperTools
+{
+    /// <summary>
+    /// 全てのアセットをインポートする
+    /// </summary>
+    public sealed class AllImporter
+    {
+        [MenuItem("GL/MasterData/Import All")]
+        private static void ImportAll()
+        {
+
+        }
+    }
+}
